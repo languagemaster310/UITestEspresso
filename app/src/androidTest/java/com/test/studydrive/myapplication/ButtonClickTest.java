b4af727a3cbcb02407d6fb2a12420a3b464db933
Amandeep Singh <amandeepsingh@ads-Book-Air.fritz.box>
2019-08-13 22:03:16 +0200

    ui test class added

---
+++ app/src/androidTest/java/com/test/studydrive/myapplication/ButtonClickTest.java
@@ -0,0 +1,13 @@
+package com.test.studydrive.myapplication;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+@LargeTest
+public class ButtonClickTest {
+
+
+
+}
